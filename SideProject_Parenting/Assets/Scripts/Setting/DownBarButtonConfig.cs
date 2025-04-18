6f6f76070de3104735ad51711e3b0430dcf9c3af
WadeLiu <wadeliu@yile.com.tw>
2025-04-18 09:24:04 +0800

    新增popupitem 設定

--- SideProject_Parenting/Assets/Scripts/Setting/DownBarButtonConfig.cs
+++ SideProject_Parenting/Assets/Scripts/Setting/DownBarButtonConfig.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Parenting.Scripts.Setting
@@ -7,5 +9,23 @@ namespace Parenting.Scripts.Setting
     {
         public string label;
         public Sprite icon;
+        public List<PopupPageConfig> popupPageConfigs;
+        //TODO 需要Dropdown的內容會不一樣
+        //TODO 需要InputField的單位不一樣
+        //TODO 時間大家都依樣
+    }
+    
+    [Serializable]
+    public class PopupPageConfig
+    {
+        public string title;
+        public PopupInfoItemType itemType;
+    }
+
+    public enum PopupInfoItemType
+    {
+        Time,
+        Dropdown,
+        InputField,
     }
 }
